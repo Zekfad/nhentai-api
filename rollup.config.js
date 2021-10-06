a7dbe353dcc27b942df25876859319a8fbfd5515
Yaroslav Vorobev <vorobevyaroslav@gmail.com>
2021-10-06 06:34:45 +0300

    chore(deps): update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import {
 } from 'path';
 
 import babelProposalClassProperties from '@babel/plugin-proposal-class-properties';
-import babel from '@rollup/plugin-babel';
+import { babel, } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
